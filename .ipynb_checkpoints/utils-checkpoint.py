5d3d61e6f3543c6d582285f1a0e64322e5b0869e
Son, Hoang Ngoc <sonhn.dev@gmail.com>
2022-01-14 17:29:42 +0700

    commit

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -34,10 +34,10 @@ def load_data(filename, train_size=0.9, random_state=13):
     return x_train, x_test, y_train, y_test
 
 
-def plot_geospartial(data, figsize, cmap='OrRd', title=None):
+def plot_geospartial(data, figsize, cmap='OrRd', title=None, vmin=None, vmax=None):
     cmap = cm.get_cmap(cmap)
-    vmin = data.min()
-    vmax = data.max()
+    vmin = vmin or data.min()
+    vmax = vmax or data.max()
     norm = Normalize(vmin=vmin, vmax=vmax)
     with open('data/states.geojson') as json_file:
         json_data = json.load(json_file)
@@ -106,14 +106,14 @@ def get_df_info(df):
     df.columns = ['Column', 'Count', 'Null count', 'Num unique', 'Type']
     display(df)
 
-# Function to calculate CHGOFF rate by feature 
-def chgoff_rate_by_feature(x_train, y_train, feature):
-    tmp = pd.concat([x_train[['LoanNr_ChkDgt', feature]], y_train], axis='columns')
-    tmp = tmp.pivot_table(index=feature,
-                          columns='MIS_Status',
-                          values='LoanNr_ChkDgt',
-                          aggfunc='count')
+def chgoff_rate_by_feature(values, y_train):
+    tmp = pd.DataFrame(y_train).pivot_table(index=values,
+                                            columns=y_train,
+                                            values='MIS_Status',
+                                            aggfunc='count')
+
     tmp = tmp['CHGOFF'] / tmp.sum(axis='columns')
+    tmp.name = 'CHGOFF_Rate'
     return tmp
 
 def date_value(date_strings, errors='coerce', max_year=2000):
@@ -137,24 +137,77 @@ def amount_value(value_strings):
         return value_string.replace(',', '')[1:]
     return value_strings.applymap(transform).astype(np.float64)
 
-def plot_hist_and_box_plot(original_amount, amount_transformed, y_train, feature, bins=20):
+def plot_hist_and_box_plot(original_amt, transformed_amt, y_train, bins=20):
+    feature = original_amt.name
+    
     fig = plt.figure(figsize=(15, 4))
+
     fig.add_subplot(1, 3, 1)
-    plt.hist(original_amount[feature],
-             bins=bins)
+    plt.hist(original_amt, bins=bins)
     plt.title(f'{feature} (original)')
     plt.xlabel('Amount')
     plt.ylabel('Count')
+
     fig.add_subplot(1, 3, 2)
-    plt.hist(amount_transformed[feature],
-             bins=bins)
+    plt.hist(transformed_amt, bins=bins)
     plt.title(f'{feature} (transformed)')
     plt.xlabel('Amount')
+
     fig.add_subplot(1, 3, 3)
     cond = (y_train=='P I F')
-    plt.boxplot([amount_transformed.loc[cond, feature],
-                 amount_transformed.loc[~cond, feature]],
+    plt.boxplot([transformed_amt[cond],
+                 transformed_amt[~cond]],
                 labels=['PIF', 'CHGOFF'])
     plt.title(f'CHGOFF rate by {feature} (transformed)')
     plt.xlabel('MIS Status')
     plt.ylabel('Amount')
+
+def plot_box(values, y_train):
+    feature = values.name
+    cond = (y_train=='P I F')
+    plt.boxplot([values[cond],
+                 values[~cond]],
+                labels=['PIF', 'CHGOFF'])
+    plt.title(f'MIS_Status by {feature}')
+
+def maturity_date_is_between(disburse_date, term, date_from, date_to):
+    month_offset = term.map(lambda x: pd.DateOffset(months=x))
+    maturity_date = disburse_date + month_offset
+    cond = ((maturity_date <= np.datetime64(date_to)) &
+            (maturity_date >= np.datetime64(date_from)))
+    return cond
+
+def plot_stacked_bars(counts, margin=0.5, width=0.4):
+    def draw_amt(data, bottom=None):
+        for i, val in enumerate(data):
+            y = val/2
+            if bottom is not None:
+                y += bottom[i]
+            plt.text(x[i], y, f'{val:.2f}', va='center', ha='center')
+    x = counts.columns
+    mis_status_1 = counts.iloc[0, :]
+    mis_status_2 = counts.iloc[1, :]
+    plt.bar(x, mis_status_1, width=width)
+    draw_amt(mis_status_1)
+    plt.bar(x, mis_status_2, width=width, bottom=mis_status_1)
+    draw_amt(mis_status_2, bottom=mis_status_1)
+    plt.xlabel(counts.columns.name)
+    plt.xticks(x, x)
+    plt.title(f'{counts.index.name} by {counts.columns.name}')
+    plt.legend(counts.index, loc='upper right')
+    plt.margins(x=0.5)
+
+def find_clusters(model, data, feature):
+    x = data.to_numpy().reshape(-1, 1)
+    y = model.fit_predict(x)
+    tmp = pd.concat([
+        data,
+        pd.Series(y, index=data.index, name='Cluster')
+    ], axis='columns')
+    tmp = tmp.reset_index()
+    tmp = tmp.groupby('Cluster', as_index=False).agg({'CHGOFF_Rate': 'max',
+                                                      feature: lambda x: x.to_list()})
+    tmp = tmp.sort_values('CHGOFF_Rate')
+    tmp = tmp.reset_index(drop=True)
+    tmp = tmp.drop(columns='Cluster')
+    return tmp.to_dict(orient='index')
